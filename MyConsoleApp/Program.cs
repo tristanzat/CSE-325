0c7d85ee05f0bdb68113d1b0b2ec40597213b0b1
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-09-16 13:39:04 -0600

    add tutorial console app

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}.");
+
+// Get days until Christmas given a certain date
+static int DaysUntilChristmas(DateTime date)
+{
+    int year = date > new DateTime(date.Year, 12, 25) ? date.Year + 1 : date.Year;
+    DateTime christmas = new(year, 12, 25);
+    var span = christmas - date;
+    int days = span.Days;
+    return days;
+}
+
+Console.WriteLine($"The next Christmas is in {DaysUntilChristmas(DateTime.Now)} days.");
